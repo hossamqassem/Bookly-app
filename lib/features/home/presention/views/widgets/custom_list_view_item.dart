a602eaa7433a69867689695ae5a20bbec9e06d4b
Hossam Qassam <122543486+hossamqassem@users.noreply.github.com>
2025-04-06 11:24:42 +0200

    `Refactor: Rename CustomListViewItem to FeaturedListViewItem and introduce FeaturedBooksListView`

--- lib/features/home/presention/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presention/views/widgets/custom_list_view_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
